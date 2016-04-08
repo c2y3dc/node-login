4b6c8a4f8ffc7226c87284f172bed89b0e79d68d
Ervin Chow <ervinchow@gmail.com>
2016-04-07 22:24:54 -0700

    adds base files, db, app config and view templates

---
+++ app/routes.js
@@ -0,0 +1,49 @@
+module.exports = function(app, passport) {
+  //Home page
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
+
+  //login
+  app.get('/login', function(req, res) {
+    //render the page and pass in any flash data if it exits
+    res.render('login.ejs', {message: req.flash('loginMessage')});
+  });
+
+  //process the login form
+  //app.post('/login', do passport stuff)
+
+  //sign up
+  app.get('/signup', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', {message: req.flash('signupMessage')});
+  });
+
+  //progess the signup form
+  //app.post('/signup', do passport stuff)
+
+  //profile section
+  //protected loggedIn to visit
+  //use route middleware to verify this (the isLoggedIn function)
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user: req.user // get the user out of session and pass to template
+    });
+  });
+
+  //logout
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+// middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  //if user is authenticated in the session, proceed
+  if (req.isAuthenticated())
+  return next();
+
+  // if not redirect to the home page
+  res.redirect('/');
+}
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url': 'mongodb://<user>:<pass>@jello.modulusmongo.net:27017/wo8qidaX'
+}
